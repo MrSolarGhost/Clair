01b003a94120d543bb1c7c2e53cf67c6a8768d80
solarghost <mrsolarghost@gmail.com>
2026-03-05 02:00:36 -0500

    feat(ui): wire collections screen to db

--- lib/screens/collections_screen.dart
+++ lib/screens/collections_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'collection_detail_screen.dart';
+import 'collection_editor_screen.dart';
+import '../services/collections_service.dart';
+import '../models/collection.dart';
 
 class CollectionsScreen extends StatefulWidget {
   const CollectionsScreen({super.key});
@@ -12,45 +15,31 @@ class CollectionsScreen extends StatefulWidget {
 class _CollectionsScreenState extends State<CollectionsScreen> {
   int _selectedIndex = 0;
 
-  // Mock collections data
-  final List<Map<String, dynamic>> _collections = [
-    {
-      'name': 'Currently Playing',
-      'gameCount': 5,
-      'color': Colors.blue,
-      'description': 'Games I\'m actively working through',
-    },
-    {
-      'name': 'Cozy Games',
-      'gameCount': 12,
-      'color': Colors.green,
-      'description': 'Relaxing games for winding down',
-    },
-    {
-      'name': 'Quick Sessions',
-      'gameCount': 8,
-      'color': Colors.orange,
-      'description': 'Perfect for short play sessions',
-    },
-    {
-      'name': 'Co-op Favorites',
-      'gameCount': 6,
-      'color': Colors.purple,
-      'description': 'Best games to play with friends',
-    },
-    {
-      'name': 'Backlog Priority',
-      'gameCount': 15,
-      'color': Colors.red,
-      'description': 'Games I want to finish next',
-    },
-    {
-      'name': 'Comfort Classics',
-      'gameCount': 10,
-      'color': Colors.amber,
-      'description': 'Games to replay when stressed',
-    },
-  ];
+  final CollectionsService _collectionsService = CollectionsService();
+  List<Collection> _collections = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCollections();
+  }
+
+  Future<void> _loadCollections() async {
+    try {
+      final collections = await _collectionsService.listCollections();
+      if (mounted) {
+        setState(() {
+          _collections = collections;
+          _isLoading = false;
+        });
+      }
+    } catch (_) {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,8 +73,16 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
               ),
               // Create new collection button
               ElevatedButton.icon(
-                onPressed: () {
-                  _showCreateCollectionDialog();
+                onPressed: () async {
+                  final created = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CollectionEditorScreen(),
+                    ),
+                  );
+                  if (created == true) {
+                    _loadCollections();
+                  }
                 },
                 icon: const Icon(Icons.add, size: 18),
                 label: const Text('New Collection'),
@@ -104,109 +101,106 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
 
           // Collections grid
           Expanded(
-            child: Focus(
-              autofocus: true,
-              onKeyEvent: _handleKeyEvent,
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 24,
-                  mainAxisSpacing: 24,
-                  childAspectRatio: 1.3,
-                ),
-                itemCount: _collections.length,
-                itemBuilder: (context, index) {
-                  final collection = _collections[index];
-                  final isSelected = index == _selectedIndex;
-
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() => _selectedIndex = index);
-                      _openCollection(collection);
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: isSelected
-                            ? (collection['color'] as Color).withValues(alpha: 0.15)
-                            : const Color(0xFF1A1A1A),
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: isSelected
-                              ? (collection['color'] as Color)
-                              : Colors.transparent,
-                          width: 2,
-                        ),
+            child: _isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : Focus(
+                    autofocus: true,
+                    onKeyEvent: _handleKeyEvent,
+                    child: GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 24,
+                        mainAxisSpacing: 24,
+                        childAspectRatio: 1.3,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          // Collection icon and name
-                          Row(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(12),
-                                decoration: BoxDecoration(
-                                  color: (collection['color'] as Color).withValues(alpha: 0.2),
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: Icon(
-                                  Icons.collections_bookmark,
-                                  color: collection['color'] as Color,
-                                  size: 28,
-                                ),
+                      itemCount: _collections.length,
+                      itemBuilder: (context, index) {
+                        final collection = _collections[index];
+                        final isSelected = index == _selectedIndex;
+
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() => _selectedIndex = index);
+                            _openCollection(collection);
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 200),
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: isSelected
+                                  ? Colors.blueGrey.withValues(alpha: 0.15)
+                                  : const Color(0xFF1A1A1A),
+                              borderRadius: BorderRadius.circular(16),
+                              border: Border.all(
+                                color: isSelected ? Colors.blueGrey : Colors.transparent,
+                                width: 2,
                               ),
-                              const SizedBox(width: 16),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
                                   children: [
-                                    Text(
-                                      collection['name'] as String,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: isSelected
-                                            ? collection['color'] as Color
-                                            : Colors.white,
+                                    Container(
+                                      padding: const EdgeInsets.all(12),
+                                      decoration: BoxDecoration(
+                                        color: Colors.blueGrey.withValues(alpha: 0.2),
+                                        borderRadius: BorderRadius.circular(12),
                                       ),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
+                                      child: const Icon(
+                                        Icons.collections_bookmark,
+                                        color: Colors.blueGrey,
+                                        size: 28,
+                                      ),
                                     ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      '${collection['gameCount']} games',
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey.shade600,
+                                    const SizedBox(width: 16),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            collection.name,
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                              color: isSelected
+                                                  ? Colors.blueGrey
+                                                  : Colors.white,
+                                            ),
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            '${collection.gameCount} games',
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey.shade600,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
-                          ),
-
-                          // Description
-                          Text(
-                            collection['description'] as String,
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey.shade500,
-                              height: 1.4,
+                                Text(
+                                  collection.description ?? '',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey.shade500,
+                                    height: 1.4,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
                             ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ),
+                  ),
           ),
         ],
       ),
@@ -218,6 +212,8 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
 
     final itemsPerRow = 3;
     final totalItems = _collections.length;
+
+    if (totalItems == 0) return KeyEventResult.ignored;
 
     if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
       setState(() {
@@ -248,26 +244,13 @@ class _CollectionsScreenState extends State<CollectionsScreen> {
     return KeyEventResult.ignored;
   }
 
-  void _openCollection(Map<String, dynamic> collection) {
+  void _openCollection(Collection collection) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => CollectionDetailScreen(
-          collectionName: collection['name'] as String,
-          collectionDescription: collection['description'] as String,
-          collectionColor: collection['color'] as Color,
-          gameCount: collection['gameCount'] as int,
+          collectionId: collection.id!,
         ),
-      ),
-    );
-  }
-
-  void _showCreateCollectionDialog() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: const Text('Create new collection (coming soon)'),
-        duration: const Duration(seconds: 2),
-        backgroundColor: Colors.blue.shade700,
       ),
     );
   }
